beea19f00a70bbec2b2ed278a95ccd7cd4baa1a6
Pavan <129081053+Pavanpraja@users.noreply.github.com>
2024-07-06 13:04:27 +0530

    do some changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ function App() {
 
   return (
     <>
-     <div className='bg-[#e2e2e27d] h-screen -mt-[0rem] overflow-x-hidden overflow-y-hidden'>
+     <div className='h-screen -mt-[0rem] overflow-x-hidden overflow-y-hidden'>
+     <div className='animate-pulse ani-bac w-[100%] h-[100%] absolute'></div>
       <Homepage />
      </div>
     </>
--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,9 +1,9 @@
 import './App.css'
 const Homepage = () => {
   return (
-    <div className="m-[2rem] flex flex-col items-center justify-center p-[1rem]">
-      <h1 className="text-[5rem] text-center font-bold text-[#676767]">Hello {"it's"} our Home page</h1>
-      <p className="text-center w-[60rem] p-[2rem] text-[2rem] font-semibold text-[#9b9a9a] max-[550px]:w-[30rem]">Welcome to our home page so let i explore you about our home page. I have Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+    <div className="m-[2rem] flex flex-col items-center justify-center p-[1rem] text-[black]">
+      <h1 className="text-[5rem] text-center font-bold [text-shadow:0_0.2rem_0.1rem_black]">Hello {"it's"} our Home page</h1>
+      <p className="text-center p-[2rem] text-[2rem] font-semibold [text-shadow:0_0.1rem_0.1rem_black]">Welcome to our home page so let i explore you about our home page. <br /> I have Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
     </div>
   )
 }
