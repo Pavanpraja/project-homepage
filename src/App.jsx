74af354445e751298f91ee8970be1ad23347b040
Pavan <129081053+Pavanpraja@users.noreply.github.com>
2024-07-06 10:09:04 +0530

    created home page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css'
+import Homepage from './Homepage'
+
+function App() {
+
+  return (
+    <>
+     <div className='bg-[#e2e2e27d] h-screen -mt-[0rem] overflow-x-hidden overflow-y-hidden'>
+      <Homepage />
+     </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Homepage.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+const Homepage = () => {
+  return (
+    <div className="m-[2rem] flex flex-col items-center justify-center p-[1rem]">
+      <h1 className="text-[5rem] text-center font-bold text-[#676767]">Hello {"it's"} our Home page</h1>
+      <p className="text-center w-[60rem] p-[2rem] text-[2rem] font-semibold text-[#9b9a9a] max-[550px]:w-[30rem]">Welcome to our home page so let i explore you about our home page. I have Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+    </div>
+  )
+}
+
+export default Homepage
